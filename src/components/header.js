ce6aaaa35b4d043e3d9205ea5b9db6186cf0cf8e
ohduran <alvaro.duranb@hotmail.com>
2020-03-15 11:29:15 +0100

    7 - styling gatsby with css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
+// import './header.module.scss';
+import headerStyles from './header.module.scss';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Alvaro</h1>
-      <ul>
-        <li><Link exact to="/">Home</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Alvaro
+        </Link>
+      </h1>
+      <ul className={headerStyles.navList}>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} exact to="/">Home</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+        <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
       </ul>
-    </div>
+    </header>
   )
 }
 
